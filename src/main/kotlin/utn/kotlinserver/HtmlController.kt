16686e145de78d60eb95dc893ca7d314054b08fa
Nicolas Coen <ncoen97@gmail.com>
2020-08-22 20:50:24 -0300

    running "blog"

---
+++ src/main/kotlin/utn/kotlinserver/HtmlController.kt
@@ -0,0 +1,18 @@
+package utn.kotlinserver
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HtmlController {
+
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Blog"
+        return "blog"
+    }
+
+}
