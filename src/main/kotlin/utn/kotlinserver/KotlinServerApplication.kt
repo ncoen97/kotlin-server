b7cc6335a1e9f6686fc8eb68374043ac5f6867b1
Nicolas Coen <ncoen97@gmail.com>
2020-08-22 19:56:08 -0300

    initial commit

---
+++ src/main/kotlin/utn/kotlinserver/KotlinServerApplication.kt
@@ -0,0 +1,11 @@
+package utn.kotlinserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinServerApplication>(*args)
+}
